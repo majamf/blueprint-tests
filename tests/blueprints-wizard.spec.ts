d438b4bc5b35f495b2049249dcacdc5c83f0a1fb
Jakub Vojacek <89916133+jakubvojacek-wandera@users.noreply.github.com>
2024-07-01 17:09:50 +0200

    TRIVIAL - Add Playwright skeleton (#2)
    * TRIVIAL - add playwright skeleton

---
+++ tests/blueprints-wizard.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+const baseUrl = 'https://blueprints.stage-mfe.jamf.io/?clusterUrl=https://tyk.sbox.ocean.jamf.build';
+
+test('has title', async ({ page }) => {
+  await page.goto(baseUrl);
+
+  await expect(page).toHaveTitle('Blueprints');
+});
