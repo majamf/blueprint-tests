49bcd1d68f976a73f6ae33fbb053c73dddafff9b
mhroziencikova <michaela.hroziencikova@wandera.com>
2025-01-09 15:43:00 +0100

    JSC-61906 Add blueprint tests

--- tests/steps/blueprints-steps.ts
+++ tests/steps/blueprints-steps.ts
@@ -116,7 +116,9 @@ export default class BlueprintsSteps {
 	}
 
 	async adminOpensScopeDrawer() {
-		const scopeCardLink = this.page.locator(blueprintCardLocator, { has: this.page.locator(`h5:has-text("Scope")`) }).getByRole('link');
+		const scopeCardLink = this.page
+			.locator(blueprintCardLocator, { has: this.page.locator(`h5:has-text("Scope")`) })
+			.getByRole('link');
 
 		await scopeCardLink.click();
 	}
