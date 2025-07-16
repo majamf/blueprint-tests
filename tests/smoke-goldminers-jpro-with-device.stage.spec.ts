5c8222a7081407e6fe1be2cee1c241ddd27e2660
Wojciech Gorczynski <gorczynski.wojciech.qa@gmail.com>
2025-07-16 08:43:44 +0200

    [JPCFM-3925]Add scenarios to smoke-goldminers-jpro-with-device.stage.spec.ts

---
+++ tests/smoke-goldminers-jpro-with-device.stage.spec.ts
@@ -0,0 +1,66 @@
+import { test } from '@playwright/test';
+import { v4 as uuidv4 } from 'uuid';
+import JProLoginSteps from './steps/jpro-login-steps';
+import BlueprintsSteps from './steps/blueprints-steps';
+import JProApiSteps from './steps/jpro-api-steps';
+import MimicSteps from './steps/mimic-steps';
+import BlueprintDetailPageSteps from './steps/blueprint-detail-page-steps';
+import NavigationSteps from './steps/navigation-steps';
+
+const baseUrl = process.env.JAMF_PRO_BASE_URL || 'https://vhdpsvhf.pyro.jamf.build/';
+
+let id = uuidv4();
+
+test.beforeEach(async () => {
+	console.log(`Running "${test.info().title}" in ${test.info().project.name}`);
+	if (test.info().retry != 0) {
+		console.log(`Running ${test.info().retry}. retry of "${test.info().title}" in ${test.info().project.name}`);
+	}
+	id = uuidv4();
+});
+
+test('Deploy blueprint containing config profile component to mimic device in Jamf Pro', { tag: ['@stage', '@mimic'] }, async ({ page }) => {
+	const jproLoginSteps = new JProLoginSteps(page);
+	const blueprintsSteps = new BlueprintsSteps(page);
+	const blueprintDetailPageSteps = new BlueprintDetailPageSteps(page);
+	const navigationSteps = new NavigationSteps(page);
+	const jproApiSteps = new JProApiSteps(baseUrl);
+	const mimicSteps = new MimicSteps();
+
+	const udid = await jproApiSteps.getMobileDeviceUdid();
+
+	await jproLoginSteps.loginToJamfPro(baseUrl);
+	await navigationSteps.adminOpensBlueprintsViaJamfProNavigation();
+	await blueprintsSteps.blueprintsPageIsOpen();
+	await blueprintsSteps.adminOpensBlueprintBuilder();
+	await navigationSteps.newBlueprintModalIsOpen();
+	await blueprintsSteps.adminFillsNameOfBlueprint('Blueprint_with_CP_e2e_' + id);
+	await blueprintsSteps.adminFillsDescriptionOfBlueprint('e2e automated test');
+	const blueprintId = await blueprintsSteps.adminClicksCreateBlueprintButton();
+
+	await blueprintDetailPageSteps.blueprintWithNameIsOpened('Blueprint_with_CP_e2e_' + id);
+	await blueprintDetailPageSteps.adminWaitsForToastToDisappear('Blueprint created');
+	await blueprintDetailPageSteps.adminOpensAddModalOfComponent('Lock Screen Message');
+	await blueprintDetailPageSteps.configProfileComponentDrawerIsOpened('Lock Screen Message');
+	await blueprintDetailPageSteps.adminClicksOnGivenCheckbox('AssetTagInformation');
+	await blueprintDetailPageSteps.adminAddsConfigurationOfComponent();
+	await blueprintDetailPageSteps.blueprintIsNotReadyForDeploymentInAnalyticsCard();
+	await blueprintDetailPageSteps.adminOpensScopeDrawer();
+	await blueprintDetailPageSteps.scopingDrawerIsOpened();
+	await blueprintDetailPageSteps.adminSelectsGroupWithNameInScope('mimic device');
+	await blueprintDetailPageSteps.adminSavesScope();
+	await blueprintDetailPageSteps.blueprintIsReadyForDeploymentInAnalyticsCard();
+	await blueprintDetailPageSteps.adminDeploysBlueprint();
+	await mimicSteps.blueprintIsDeployedToMimicDeviceViaJamfPro(
+		blueprintId,
+		udid,
+		'com.apple.configuration.legacy'
+	);
+
+	await blueprintDetailPageSteps.adminReloadsTheBlueprintDetailsPage();
+	await blueprintDetailPageSteps.blueprintWithNameIsOpened('Blueprint_with_CP_e2e_' + id);
+	await blueprintDetailPageSteps.thereAreDeployedDevicesInAnalytics(1);
+
+	await blueprintDetailPageSteps.adminDeletesBlueprint();
+	await blueprintsSteps.thereIsNoBlueprintWithName('Blueprint_with_CP_e2e_' + id);
+});
