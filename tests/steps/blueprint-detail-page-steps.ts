9df4c038c8a4479c2305a309b9146b3646ae86ad
Wojciech Gorczynski <gorczynski.wojciech.qa@gmail.com>
2025-08-14 13:33:53 +0200

    [JPCFM-3925] break test to verify notify_failure notification

--- tests/steps/blueprint-detail-page-steps.ts
+++ tests/steps/blueprint-detail-page-steps.ts
@@ -185,7 +185,7 @@ export default class BlueprintDetailPageSteps {
 
 	@Step('Selected option with name "$0" is checked')
 	async selectedCheckboxIsChecked(payloadKey: string) {
-		await expect(this.page.getByTestId(`${payloadKey}`).locator('input').first()).toBeChecked();
+		await expect(this.page.getByTestId(`${payloadKey}`).locator('input').first()).toBeDisabled();
 	}
 
 	@Step('No config profile component payload key matches given filter option')
