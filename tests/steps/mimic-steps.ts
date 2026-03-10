25d3988973ce57a5983d9693ff85c3f74d122fa7
Vojtech Kozel <vojtech.kozel@jamf.com>
2026-03-10 13:16:25 +0100

    OCEAN-266 - Change activation identifiers name

--- tests/steps/mimic-steps.ts
+++ tests/steps/mimic-steps.ts
@@ -111,7 +111,7 @@ export default class MimicSteps {
 	@Step('Blueprint with id "$0" is deployed to mimic device "$1" with type "$2" via Jamf Pro')
 	public async blueprintIsDeployedToMimicDeviceViaJamfPro(blueprintId: UUID, udid: string, type: string) {
 		const configurationIdentifier = `Blueprint_${blueprintId}_s1_c1_sys_cfg1`;
-		const activationIdentifier = `Blueprint_${blueprintId}_s1_sys_act1`;
+		const activationIdentifier = `Blueprint_${blueprintId}_s1_c1_sys_act1`;
 
 		await this.pollMimicConfigurations(udid, configurationIdentifier);
 
@@ -125,7 +125,7 @@ export default class MimicSteps {
 	@Step('Blueprint with id "$0" is deployed to mimic device "$1" with type "$2" via Jamf School')
 	public async blueprintIsDeployedToMimicDeviceViaJamfSchool(blueprintId: UUID, udid: string, type: string) {
 		const configurationIdentifier = `Blueprint_${blueprintId}_s1_c1_sys_cfg1`;
-		const activationIdentifier = `Blueprint_${blueprintId}_s1_sys_act1`;
+		const activationIdentifier = `Blueprint_${blueprintId}_s1_c1_sys_act1`;
 
 		await this.pollMimicConfigurationsWithCheckIn(udid, configurationIdentifier);
 
